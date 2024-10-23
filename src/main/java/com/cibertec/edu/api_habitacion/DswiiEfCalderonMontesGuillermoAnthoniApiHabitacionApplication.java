44676d7740de24c2e1390551e8b3987d16db4b19
GuilleCalM <guilleacalderon@hotmail.com>
2024-10-23 18:16:05 -0500

    first commit

---
+++ src/main/java/com/cibertec/edu/api_habitacion/DswiiEfCalderonMontesGuillermoAnthoniApiHabitacionApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.edu.api_habitacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiEfCalderonMontesGuillermoAnthoniApiHabitacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DswiiEfCalderonMontesGuillermoAnthoniApiHabitacionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cibertec/edu/api_habitacion/DswiiEfCalderonMontesGuillermoAnthoniApiHabitacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.edu.api_habitacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiEfCalderonMontesGuillermoAnthoniApiHabitacionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
